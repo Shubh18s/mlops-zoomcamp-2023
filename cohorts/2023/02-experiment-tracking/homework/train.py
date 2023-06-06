300960fc8627ac6fcc11678899cc305cb5e50a46
Shubhdeep Singh <singh18shubhdeep@gmail.com>
2023-06-06 09:44:02 +0000

    merge-repo

--- cohorts/2023/02-experiment-tracking/homework/train.py
+++ cohorts/2023/02-experiment-tracking/homework/train.py
@@ -5,6 +5,8 @@ import click
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
+import mlflow
+
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -22,8 +24,15 @@ def run_train(data_path: str):
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
     X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
 
+    mlflow.set_tracking_uri("sqlite:///mlflow-hw.db")
+    mlflow.set_experiment("nyc-green-taxi-autolog-experiment")
+
+    mlflow.sklearn.autolog(log_models=True)
+
     rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
+    with mlflow.start_run():
+        rf.fit(X_train, y_train)
+    
     y_pred = rf.predict(X_val)
 
     rmse = mean_squared_error(y_val, y_pred, squared=False)
